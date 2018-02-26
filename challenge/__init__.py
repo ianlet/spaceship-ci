76142e1915eafc5af7085ae2f1aaa1c593d654c5
Ian Létourneau <letourneau.ian@gmail.com>
2018-02-25 23:07:38 -0500

    Delegate to assignment to clone or update itself

--- challenge/__init__.py
+++ challenge/__init__.py
@@ -16,35 +16,7 @@ class Challenge:
         self.__ensure_exists(challenge_path)
 
         for assignment in self.assignments:
-            assignment_path = f'{challenge_path}/{assignment.name}'
-            self.__clone_or_update_assignment(assignment, assignment_path)
-
-    def __clone_or_update_assignment(self, assignment, assignment_path):
-        if os.path.exists(assignment_path):
-            self.__update_assignment(assignment, assignment_path)
-        else:
-            self.__clone_assignment(assignment, assignment_path)
-
-    def __clone_assignment(self, assignment, assignment_path):
-        print(f'Cloning assignment {assignment.name} into {assignment_path}...')
-        assignment_repo = Repo.clone_from(assignment.url, assignment_path)
-        print(f'Assignment cloned!')
-
-        print(f'Checking out {assignment.name} to master...')
-        assignment_repo.heads.master.checkout()
-
-    def __update_assignment(self, assignment, assignment_path):
-        print(f'Assignment {assignment.name} already exists at {assignment_path}')
-
-        print(f'Checking out {assignment.name} to master...')
-        assignment_repo = Repo(assignment_path)
-        assignment_repo.heads.master.checkout()
-
-        print(f'Updating assignment {assignment.name}...')
-        origin = assignment_repo.remote('origin')
-        origin.pull(['--ff', '--force'])
-
-        print(f'Assignment updated!')
+            assignment.clone_or_update(challenge_path)
 
     def __ensure_exists(self, challenge_path):
         if not os.path.exists(challenge_path):
@@ -55,3 +27,30 @@ class Assignment:
     def __init__(self, name, url):
         self.name = name
         self.url = url
+
+    def clone_or_update(self, base_path):
+        path = f'{base_path}/{self.name}'
+        if os.path.exists(path):
+            print(f'Assignment {self.name} already exists at {path}')
+            self.__update(path)
+        else:
+            self.__clone(path)
+
+    def __clone(self, path):
+        print(f'Cloning assignment {self.name} into {path}...')
+        assignment_repo = Repo.clone_from(self.url, path)
+        print(f'Assignment cloned!')
+
+        print(f'Checking out {self.name} to master...')
+        assignment_repo.heads.master.checkout()
+
+    def __update(self, path):
+        print(f'Checking out {self.name} to master...')
+        assignment_repo = Repo(path)
+        assignment_repo.heads.master.checkout()
+
+        print(f'Updating assignment {self.name}...')
+        origin = assignment_repo.remote('origin')
+        origin.pull(['--ff', '--force'])
+
+        print(f'Assignment updated!')
