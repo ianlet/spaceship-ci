06b85765cf8550460b2575e39e031c91693c02c7
Ian Létourneau <letourneau.ian@gmail.com>
2018-03-01 01:16:54 -0500

    Create and run container to test submission in test stage

--- src/challenge/__init__.py
+++ src/challenge/__init__.py
@@ -1,7 +1,9 @@
-from .build_stage import BuildStage
 from .challenge import Challenge
 from .challenge_verifier import ChallengeVerifier
-from .preparation_stage import PreparationStage
 from .submission import Submission
 from .verification_pipeline import VerificationPipeline
+
 from .verification_stage import VerificationStage
+from .preparation_stage import PreparationStage
+from .build_stage import BuildStage
+from .test_stage import TestStage
--- src/challenge/test_stage.py
+++ src/challenge/test_stage.py
@@ -1,15 +1,15 @@
 from challenge import VerificationStage
 
 
-class BuildStage(VerificationStage):
+class TestStage(VerificationStage):
     def __init__(self, base_path, executor):
-        super().__init__('build')
+        super().__init__('test')
         self.base_path = base_path
         self.executor = executor
 
     def execute(self, submission):
         container_name = f'{submission.slug}--{self.name}'
-        container_command = ['./gradlew', 'build', '-x', 'test']
+        container_command = ['./gradlew', 'test']
         submission_path = f'{self.base_path}/{submission.path}'
 
         container = self.executor.create_container(submission.slug, container_name, container_command, submission_path)
